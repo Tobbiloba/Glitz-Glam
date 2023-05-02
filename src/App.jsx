2c4b1a443efc263ce67d1aa8cec4ea3e50efbcd3
Tobbiloba <salauoluwatobiloba0@gmail.com>
2023-05-02 12:49:35 +0100

    removed unnecessary text

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
       <HomePage />
       <div>
         <section className="hero-image">
-          <h2>Welcome to Glitz&Glam Pink Lips Accessories</h2>
+          <h2>Welcome to Glitz&Glam Accessories</h2>
           <p>
             We offer a range of lip care products that will help you achieve the
             perfect pink lips.
